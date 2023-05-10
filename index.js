671067e920a954aa661f15ca1f6d39c3e49c3ebe
SERGIO LUIZ MORAL MARQUES <P226024@acd.puc-campinas.edu.br>
2023-05-10 09:11:38 -0300

    aula 1005

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const multi = require('./multiply')
+var express = require('express')
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
+const server = express()
+
+server.listen(PORT, () => console.log('Escutando em ${PORT}'));
+
+server.get('/', function(req,res){
+    res.send("Site de tecnologia 1");
+});
+
+server.get('/ht', (req,res) => res.sendFile(INDEX, { root: __dirname}));
+
+const multi = require('./multiply')
 console.log(multi.multiply('3,4'));
