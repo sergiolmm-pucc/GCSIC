a5b0e930bdd148289b6abc9dd560a8257fa7f548
sergiomarques <sergio@unicamp.br>
2023-05-16 18:00:34 -0300

    teste ok

--- tests/button-render.js
+++ tests/button-render.js
@@ -1,4 +1,5 @@
 const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 const {Builder, Browser, By } = require('selenium-webdriver');
  
 const screen = {
@@ -7,18 +8,27 @@ const screen = {
 };
  
 (async function buttonRender() {
-  let driver = await new Builder()
-    .forBrowser(Browser.CHROME)
-    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
-    .build();
- 
+    console.log("BUILD 1");
+    let driver = await new Builder()
+      .forBrowser(Browser.CHROME)
+    //.setChromeOptions(new chrome.Options().headless().windowSize(screen))
+      .build();
+    console.log("BUILD 2");
   try {
-    await driver.get('<http://localhost:3000>');
-    let didSendButtonRender = await driver.findElement(By.id('sendbutton')).isDisplayed()
+    await driver.get('http://localhost:3000/ht');
+    
+    let btn = await driver.findElement(By.id('sendbutton'));   
+    let didSendButtonRender = btn.isDisplayed()
     if (!didSendButtonRender){
       throw new Error(`Send button was not rendered properly.`);
     }
-  
+    console.log("BUILD 3");
+    
+        //let b = await driver.findElement(By.id('sendbutton'));
+    let text = await driver.findElement(By.id('sendbutton')).getText().then((text) =>{ return "ok - " + text});   
+    console.log(text);
+
+
   } finally {
     await driver.quit();
   }
