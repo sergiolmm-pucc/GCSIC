7698896b588df163b8d055c1cffbad419b07f1a7
sergiomarques <sergio@unicamp.br>
2023-05-16 23:37:35 -0300

    teste ok 5

---
+++ tests/onsite.js
@@ -0,0 +1,69 @@
+const process = require('process');
+const webdriver = require('selenium-webdriver');
+const {until} = require('selenium-webdriver');
+
+//describe('sampleFunctionalTests', function ()
+(async function buttonRender() 
+ {
+    console.log("BUILD 1");
+	//this.timeout(0);
+
+	
+	var capabilities = webdriver.Capabilities.chrome();
+	capabilities.set('chromeOptions', {'args': ['--no-sandbox']});
+
+	let driver = new webdriver.Builder()
+			.forBrowser('chrome')
+			.withCapabilities(capabilities)
+			.build();
+	await driver.manage().setTimeouts({pageLoad: 300000});
+	
+
+    try {
+        //await driver.get('http://localhost:3000/ht');
+        
+        console.log("BUILD 3");
+        //it('Assert page title', async() => {
+            var startTimestamp = Date.now()
+            var endTimestamp = startTimestamp + 20 * 1000; // 20 segundos
+            var tentativa = 0;
+            while(true)
+            {
+                tentativa = tentativa + 1;
+                console.log("While ");
+                try
+                {
+                    await driver.get('https://billowy-flowery-cousin.glitch.me/ht'); //process.env['webAppUrl']
+                    await driver.wait(until.titleIs('On Site Teste'), 2000);
+                    console.log("apos wait 2000");
+                    break;
+                }
+                catch(err)
+                {
+                    console.log("no erro");
+                    var currentTimestamp = Date.now();
+                    console.log(currentTimestamp - endTimestamp);
+                    if(currentTimestamp > endTimestamp)
+                    {
+                        console.log("##vso[task.logissue type=error;]Failed with error " + err)
+                        throw new Error('Failed with error ' + err);
+                    }
+                    if (tentativa > 5){
+                        throw new Error('Title was not rendered properly.');
+                    }
+                    await new Promise(resolve=>{
+                        setTimeout(resolve,5000)
+                    });
+                }
+            }
+            console.log("Fora While");
+        //});    
+       
+      } finally {
+        console.log('Finalizado');
+        await driver.quit();//.then( () => done()).catch( () => {done();});
+      }
+
+    
+	
+})();
