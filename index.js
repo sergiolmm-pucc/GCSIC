0e37873d23b3f0be1a493f4135620b817e59d17f
ARTUR BUSANI XAVIER <artur.bx@puccampinas.edu.br>
2023-05-31 10:02:13 -0300

    markup2 pagina

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var express = require('express')
-const { markupGet } = require('./markup2')
+const { markupGet } = require('./mkp2/markup2')
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const endpoints = {
-    etec: 'etec/etec.html',
-    etec2: 'etec2/etec2.html',
-    infs: 'infs/infs.html',
-    infs2: 'infs2/infs2.html',
-    infp: 'infp/infp.html',
-    infp2: 'infp2/infp2.html',
-    mkp: 'mkp/mkp.html',
-    mkp2: 'mkp2/mkp2.html',
-    teste: 'endpoints/teste.html'
+    etec: '/etec/etec.html',
+    etec2: '/etec2/etec2.html',
+    infs: '/infs/infs.html',
+    infs2: '/infs2/infs2.html',
+    infp: '/infp/infp.html',
+    infp2: '/infp2/infp2.html',
+    mkp: '/mkp/mkp.html',
+    mkp2: '/mkp2/mkp2.html',
+    teste: '/endpoints/teste.html'
 }
 
 const server = express()
 
-server.listen(PORT, () => console.log('Escutando em ${PORT}'));
+server.listen(PORT, () => console.log(`Escutando em http://localhost:${PORT}`));
 
 server.get('/', function(req,res){
     res.sendFile(base.html);
@@ -24,13 +24,13 @@ server.get('/', function(req,res){
 
 server.get('/ht', (req,res) => res.sendFile(INDEX, { root: __dirname}));
 
-server.get('/ETEC', (req, res) => res.sendFile(endpoints.etec));
-server.get('/ETEC2', (req, res) => res.sendFile(endpoints.etec2));
-server.get('/INFS', (req, res) => res.sendFile(endpoints.infs));
-server.get('/INFS2', (req, res) => res.sendFile(endpoints.infs2));
-server.get('/INFP', (req, res) => res.sendFile(endpoints.infp));
-server.get('/INFP2', (req, res) => res.sendFile(endpoints.infp2));
-server.get('/MKP', (req, res) => res.sendFile(endpoints.mkp));
-server.get('/MKP2', (req, res) => res.sendFile(endpoints.mkp2));
-server.get('/TESTE', (req, res) => res.sendFile(endpoints.teste));
+server.get('/ETEC', (req, res) => res.sendFile(endpoints.etec, { root: __dirname}));
+server.get('/ETEC2', (req, res) => res.sendFile(endpoints.etec2, { root: __dirname}));
+server.get('/INFS', (req, res) => res.sendFile(endpoints.infs, { root: __dirname}));
+server.get('/INFS2', (req, res) => res.sendFile(endpoints.infs2, { root: __dirname}));
+server.get('/INFP', (req, res) => res.sendFile(endpoints.infp, { root: __dirname}));
+server.get('/INFP2', (req, res) => res.sendFile(endpoints.infp2, { root: __dirname}));
+server.get('/MKP', (req, res) => res.sendFile(endpoints.mkp, { root: __dirname}));
+server.use('/MKP2', express.static('mkp2'))
+server.get('/TESTE', (req, res) => res.sendFile(endpoints.teste, { root: __dirname}));
 server.get('/MKP2calc', markupGet)
