f83afd61fdc0a15d9ef2a0ab989f3559de1e9ad3
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2023-06-07 09:06:24 -0300

    Update index.js
    feat: new endpoint to see ETEC page
    chore: fixed console.log PORT

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var express = require('express')
+var express = require('express');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const ETEC = '/etec.html';
 
-const server = express()
+const server = express();
 
-server.listen(PORT, () => console.log('Escutando em ${PORT}'));
+server.listen(PORT, () => console.log(`Escutando em ${PORT}`));
 
-server.get('/', function(req,res){
+server.get('/', function(req,res) {
     res.send("Site de tecnologia 1");
 });
 
 server.get('/ht', (req,res) => res.sendFile(INDEX, { root: __dirname}));
+
+server.get('/etec', (req,res) => res.sendFile(ETEC, { root: __dirname}));
 
 const multi = require('./multiply')
 console.log(multi.multiply('3,4'));
