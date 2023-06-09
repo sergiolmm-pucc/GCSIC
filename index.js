a59a44b969233d2bfbdc88e6477980cd8eb011ae
blemess <brunolemess@hotmail.com>
2023-06-09 00:24:27 -0300

    Correção de acentuação

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express')
-const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -35,6 +35,6 @@ server.get('/INFS', (req, res) => res.sendFile(endpoints.infs, { root: __dirname
 server.get('/INFS2', (req, res) => res.sendFile(endpoints.infs2, { root: __dirname}));
 server.get('/INFP', (req, res) => res.sendFile(endpoints.infp, { root: __dirname}));
 server.get('/INFP2', (req, res) => res.sendFile(endpoints.infp2, { root: __dirname}));
+server.get('/MKP', (req, res) => res.sendFile(endpoints.mkp, { root: __dirname}));
 server.use('/MKP', express.static('mkp'))
-server.use('/MKP2', express.static('mkp2'))
 server.get('/TESTE', (req, res) => res.sendFile(endpoints.teste, { root: __dirname}));
