e0903304c0534c9927dc01e819939eaf560b2038
Matheus Paiva <107510717+matheuspaiva3@users.noreply.github.com>
2023-06-14 08:37:27 -0300

    Update infp.test.js
    outras taxas

--- infp/test/infp.test.js
+++ infp/test/infp.test.js
@@ -6,4 +6,30 @@ describe("Testando o valor total do produto com imposto", () => {
       expect(totalPrice).toBe(1100);
     });
   });
-  
+  describe("Testando o valor total do produto com imposto", () => {
+    const tax = 5;
+    const productPrice = 200;
+    const totalPrice = productPrice + productPrice * (tax / 100);
+
+    test("Testando se o valor total do produto está correto", () => {
+        expect(totalPrice).toBe(210);
+    });
+});
+describe("Testando o valor total do produto com imposto", () => {
+    const tax = 15;
+    const productPrice = 50;
+    const totalPrice = productPrice + productPrice * (tax / 100);
+
+    test("Testando se o valor total do produto está correto", () => {
+        expect(totalPrice).toBe(57.5);
+    });
+});
+describe("Testando o valor total do produto com imposto", () => {
+    const tax = 0;
+    const productPrice = 80;
+    const totalPrice = productPrice + productPrice * (tax / 100);
+
+    test("Testando se o valor total do produto está correto", () => {
+        expect(totalPrice).toBe(80);
+    });
+});
