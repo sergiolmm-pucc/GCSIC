97f566c46370e950325a9d72b221669d01a9fd29
Matheus Paiva <107510717+matheuspaiva3@users.noreply.github.com>
2023-06-14 08:44:54 -0300

    Update infp.test.js
    gerador de nf

--- infp/test/infp.test.js
+++ infp/test/infp.test.js
@@ -33,3 +33,33 @@ describe("Testando o valor total do produto com imposto", () => {
         expect(totalPrice).toBe(80);
     });
 });
+describe("Gerador de Nota Fiscal", () => {
+  beforeEach(() => {
+    // Preparação dos elementos necessários para o teste, se necessário
+  });
+
+  test("Gerar Nota Fiscal com valores válidos", () => {
+    // Simulando a interação do usuário
+    document.getElementById("region").value = "SP";
+    document.getElementById("product-name").value = "Exemplo de Produto";
+    document.getElementById("product-price").value = "100";
+
+    // Chamando a função para gerar a nota fiscal
+    generateInvoice();
+
+    // Obtendo o elemento da nota fiscal gerada
+    const invoiceElement = document.getElementById("invoice");
+
+    // Realizando asserções para verificar se a nota fiscal foi gerada corretamente
+    expect(invoiceElement.innerHTML).toContain("Exemplo de Produto");
+    expect(invoiceElement.innerHTML).toContain("Região: SP");
+    expect(invoiceElement.innerHTML).toContain("Imposto: 8%");
+    expect(invoiceElement.innerHTML).toContain("Preço Total: R$ 108.00");
+  });
+
+  // Adicione outros testes aqui, se necessário
+
+  afterEach(() => {
+    // Limpeza dos elementos e estado após o teste, se necessário
+  });
+});
