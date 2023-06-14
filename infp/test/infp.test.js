ba9212117d26e4c79c6c0b227c915ae338d9841a
Matheus Paiva <107510717+matheuspaiva3@users.noreply.github.com>
2023-06-14 09:21:26 -0300

    Update infp.test.js

--- infp/test/infp.test.js
+++ infp/test/infp.test.js
@@ -3,26 +3,13 @@ describe("Testando o valor total do produto com imposto", () => {
     const productPrice = 100;
     const totalPrice = productPrice + productPrice * tax;
     test("Testando se o valor total do produto está correto", () => {
-      expect(totalPrice).toBe(1100);
-    });
-  });
-describe("Testando a atualização da taxa", () => {
-    test("Testando se a taxa é atualizada corretamente para São Paulo", () => {
-      document.getElementById("region").value = "SP";
-      updateTax();
-      expect(document.getElementById("tax-label").textContent).toBe("(8%)");
-    });
-
-    test("Testando se a taxa é atualizada corretamente para Rio de Janeiro", () => {
-      document.getElementById("region").value = "RJ";
-      updateTax();
-      expect(document.getElementById("tax-label").textContent).toBe("(6%)");
+      expect(totalPrice).toBe(1000);
     });
   });
 describe("Testando a geração da nota fiscal", () => {
     test("Testando se a nota fiscal é gerada corretamente", () => {
-      document.getElementById("region").value = "MG";
-      document.getElementById("product-name").value = "Produto Teste";
+      document.getElementById("region").value = "SP";
+      document.getElementById("product-name").value = "Produto 1";
       document.getElementById("product-price").value = "100";
   
       generateInvoice();
@@ -30,7 +17,41 @@ describe("Testando a geração da nota fiscal", () => {
       const invoiceContent = document.getElementById("invoice").innerHTML;
   
       expect(invoiceContent).toContain("<h2>Nota Fiscal</h2>");
-      expect(invoiceContent).toContain("<p>Produto: Produto Teste</p>");
+      expect(invoiceContent).toContain("<p>Produto: Produto 1</p>");
+      expect(invoiceContent).toContain("<p>Região: SP <span id=\"tax-label\">(8%)</span></p>");
+      expect(invoiceContent).toContain("<p>Imposto: 8%</p>");
+      expect(invoiceContent).toContain("<p>Preço Total: R$ 108.00</p>");
+    });
+  });
+describe("Testando a geração da nota fiscal", () => {
+    test("Testando se a nota fiscal é gerada corretamente", () => {
+      document.getElementById("region").value = "RJ";
+      document.getElementById("product-name").value = "Produto 2";
+      document.getElementById("product-price").value = "100";
+  
+      generateInvoice();
+  
+      const invoiceContent = document.getElementById("invoice").innerHTML;
+  
+      expect(invoiceContent).toContain("<h2>Nota Fiscal</h2>");
+      expect(invoiceContent).toContain("<p>Produto: Produto 2</p>");
+      expect(invoiceContent).toContain("<p>Região: RJ <span id=\"tax-label\">(6%)</span></p>");
+      expect(invoiceContent).toContain("<p>Imposto: 6%</p>");
+      expect(invoiceContent).toContain("<p>Preço Total: R$ 106.00</p>");
+    });
+  });
+describe("Testando a geração da nota fiscal", () => {
+    test("Testando se a nota fiscal é gerada corretamente", () => {
+      document.getElementById("region").value = "MG";
+      document.getElementById("product-name").value = "Produto 3";
+      document.getElementById("product-price").value = "100";
+  
+      generateInvoice();
+  
+      const invoiceContent = document.getElementById("invoice").innerHTML;
+  
+      expect(invoiceContent).toContain("<h2>Nota Fiscal</h2>");
+      expect(invoiceContent).toContain("<p>Produto: Produto 3</p>");
       expect(invoiceContent).toContain("<p>Região: MG <span id=\"tax-label\">(7%)</span></p>");
       expect(invoiceContent).toContain("<p>Imposto: 7%</p>");
       expect(invoiceContent).toContain("<p>Preço Total: R$ 107.00</p>");
