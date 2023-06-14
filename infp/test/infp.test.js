88b3bf2524d0d28e7d95429ff355d69a5334c346
Matheus Paiva <107510717+matheuspaiva3@users.noreply.github.com>
2023-06-14 08:58:29 -0300

    Update infp.test.js

--- infp/test/infp.test.js
+++ infp/test/infp.test.js
@@ -6,4 +6,12 @@ describe("Testando o valor total do produto com imposto", () => {
       expect(totalPrice).toBe(1100);
     });
   });
-
+describe("Testando a atualização da taxa", () => {
+    test("Testando se a taxa é atualizada corretamente para São Paulo", () => {
+      // Selecionando a opção de São Paulo (8%)
+      document.getElementById("region").value = "SP";
+      // Atualizando a taxa
+      updateTax();
+      // Verificando se a taxa exibida é 8%
+      expect(document.getElementById("tax-label").textContent).toBe("(8%)");
+    });
